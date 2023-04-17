e700ad68e22c55f6cec30ecc2f88d38b83179f38
João Ferreira <61028493+warneimer@users.noreply.github.com>
2023-04-17 12:50:41 +0100

    added else statement

--- SoTClient/GameLogic/PlayerLogic/Player.lua
+++ SoTClient/GameLogic/PlayerLogic/Player.lua
@@ -19,17 +19,18 @@ function Player.readPlayer()
         Player.ability3 = nil
         Player.ability4 = nil
         return false
+    else
+        Player.currenthp = player.hp
+        Player.currentessence = player.essence
+        Player.atk = (player.str)+(player.dex)+(player.itl)
+        Player.maxhp = Round(5*((player.str*0.5)+(player.dex*0.39)+(player.itl*0.23))) + 10
+        Player.maxessence = Round(5*((player.str*0.1)+(player.dex*0.12)+(player.itl*0.87)))
+        Player.ability1 = player.ability1
+        Player.ability2 = player.ability2
+        Player.ability3 = player.ability3
+        Player.ability4 = player.ability4
+        return true
     end
-    Player.currenthp = player.hp
-    Player.currentessence = player.essence
-    Player.atk = (player.str)+(player.dex)+(player.itl)
-    Player.maxhp = math.Round(5*((player.str*0.5)+(player.dex*0.39)+(player.itl*0.23))) + 10
-    Player.maxessence = math.Round(5*((player.str*0.1)+(player.dex*0.12)+(player.itl*0.87)))
-    Player.ability1 = player.ability1
-    Player.ability2 = player.ability2
-    Player.ability3 = player.ability3
-    Player.ability4 = player.ability4
-    return true
 end
 
 return Player
